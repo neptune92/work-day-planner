1ddddee88296b1579e00b797a97f910815307e52
neptune <angelacr@ymail.com>
2021-02-24 16:32:20 -0600

    adding a folder that holds a css and js file also adding a html file

---
+++ assets/moment.js
@@ -0,0 +1,26 @@
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
+
+const hour8 = $("#8");
+const hour9 = $("#9");
+const hour10 = $("#10");
+const hour11 = $("#11");
+const hour12 = $("#12");
+const hour1 = $("#13");
+const hour2 = $("#14");
+const hour3 = $("#15");
+const hour4 = $("#16");
+const hour5 = $("#17");
+
+
+
+
+//when save is clicked on it will save to local storage from text area
+const save = $(".saveBtn");
+
+save.on("click", function() {
+
+});
+
+
+//save to local storage that is in textarea
